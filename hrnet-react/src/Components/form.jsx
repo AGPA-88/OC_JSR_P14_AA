01f7a5c0a2674ad5767c6bebcc31407f1a6928fd
Antonio Alexandre <antonio.alexandre@marte.fr>
2023-06-26 17:49:18 +0200

    - Address section created;

--- hrnet-react/src/Components/form.jsx
+++ hrnet-react/src/Components/form.jsx
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import './form.css'
 
 function Form () {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+      });
+
+      function saveButton(event) {
+        console.log("Input First Name -> " + document.getElementById('firstName').value, "Input Last Name -> " + document.getElementById('lastName').value);
+        console.log("Input Street -> " + document.getElementById('street').value, "Input City -> " + document.getElementById('city').value, "Input Zip Code -> " + document.getElementById('zip-code').value);
+        // console.log("FORM DATA " + formData.firstName, "FORM DATA " + formData.lastName);
+        event.preventDefault();
+    }
+
     return (
         <div className="form-container">
         <form>
             <div>
                 First Name
-                <input type="text" name="firstName" id="firstName"/>
+                <input type="text" name="firstName" id="firstName" value={formData.firstName}/>
             </div>
             <div>
                 Last Name
-                <input type="text" name="lastName" id="lastName"/>
+                <input type="text" name="lastName" id="lastName" value={formData.lastName}/>
             </div>
+            <div className="adress-container">
+                ADDRESS
+            <div>
+                Street
+                <input type="text" name="street" id="street"/>
+            </div>
+            <div>
+                City
+                <input type="text" name="city" id="city"/>
+            </div>
+            <div>
+                Zip Code
+                <input type="text" name="zip-code" id="zip-code"/>
+            </div>
+            </div>
+        </form>
             <div>
                 <button type="submit" onClick={saveButton}>SAVE</button>
             </div>
-        </form>
         </div>
     )
-    function saveButton(event) {
-        console.log("Input First Name -> " + document.getElementById('firstName').value, "Input Last Name -> " + document.getElementById('lastName').value)
-        event.preventDefault();
-    }
 }
 
 export default Form;
