6da44fd87d244fe36ab5003d1e0448aa6cf2f13f
Antonio Alexandre <antonio.alexandre@marte.fr>
2023-07-06 16:21:55 +0200

    FormPage created;

---
+++ hrnet-react/src/Pages/formPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Form from '../Components/Form/form.jsx';
+
+function formPage() {
+    return (
+        <div>
+            <Form />
+        </div>
+    );
+}
+
+export default formPage;
