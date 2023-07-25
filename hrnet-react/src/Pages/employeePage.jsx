dfc046b2ae686a26c4d6aa423d379668f5f3872f
Antonio Alexandre <antonio.alexandre@marte.fr>
2023-07-25 20:26:36 +0200

    material table to material React table migration for dependencies compatibility

--- hrnet-react/src/Pages/employeePage.jsx
+++ hrnet-react/src/Pages/employeePage.jsx
@@ -1,68 +1,25 @@
-import React from 'react';
-import MaterialTable from 'material-table'
+import React, { useMemo } from 'react';
+import { MaterialReactTable } from 'material-react-table';
 import { useSelector } from 'react-redux';
-import { ThemeProvider, createTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import './employee.css'
 
-// ICONS //
-
-import { forwardRef } from 'react';
-
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-
-const tableIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-  };
-
-  // ICONS END //
-
-  const defaultMaterialTheme = createTheme();
-
-function EmployeePage() {
+  function EmployeePage() {
     const employees = useSelector((state) => state.employees);
 
-    const columns = [
-        { title: 'First Name', field: 'firstName' },
-        { title: 'Last Name', field: 'lastName' },
-        { title: 'Starting Date', field: 'startDate' },
-        { title: 'Department', field: 'department' },
-        { title: 'Date of Birth', field: 'dateOfBirth' },
-        { title: 'Street', field: 'street' },
-        { title: 'City', field: 'city' },
-        { title: 'US State', field: 'usState' },
-        { title: 'Zip Code', field: 'zipCode' }
-    ];
+    const columns = useMemo(
+        () => [
+        { header: 'First Name', accessorKey: 'firstName', size: 100},
+        { header: 'Last Name', accessorKey: 'lastName', size: 100 },
+        { header: 'Starting Date', accessorKey: 'startDate', size: 70 },
+        { header: 'Department', accessorKey: 'department', size: 100 },
+        { header: 'Date of Birth', accessorKey: 'dateOfBirth', size: 70 },
+        { header: 'Street', accessorKey: 'street', size: 150 },
+        { header: 'City', accessorKey: 'city', size: 150 },
+        { header: 'US State', accessorKey: 'usState', size: 150 },
+        { header: 'Zip Code', accessorKey: 'zipCode', size: 50 }
+    ],
+    [],);
 
     let keyIndex = 0;
     const data = employees.map(employee => {
@@ -77,14 +34,13 @@ function EmployeePage() {
         <h1>
             Current Employees
         </h1>
-        <ThemeProvider theme={defaultMaterialTheme}>
-            <MaterialTable
-                icons={tableIcons}
+
+            <MaterialReactTable
                 columns={columns}
                 data={data}
                 title= ""
             />
-        </ThemeProvider>
+
         <div className='home-link'>
                 <Link to="/">
                     Home
